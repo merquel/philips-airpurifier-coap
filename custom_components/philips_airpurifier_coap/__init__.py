ea9c151900b80f98f3a82adf244412dcc0051da8
kongo09 <kongo09@users.noreply.github.com>
2024-11-22 21:57:26 +0100

    remove coordinator first refresh

--- custom_components/philips_airpurifier_coap/__init__.py
+++ custom_components/philips_airpurifier_coap/__init__.py
@@ -184,8 +184,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = config_entry_data
 
-    await coordinator.async_first_refresh()
-    _LOGGER.debug("coordinator did first refresh for host %s", host)
+    # await coordinator.async_first_refresh()
+    # _LOGGER.debug("coordinator did first refresh for host %s", host)
 
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
