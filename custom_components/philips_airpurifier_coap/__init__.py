8cd04f862b0f1289e08fdb4dddb0a8b0d8b8317d
kongo09 <kongo09@users.noreply.github.com>
2024-12-02 10:50:05 +0100

    fix awaiting entry setup

--- custom_components/philips_airpurifier_coap/__init__.py
+++ custom_components/philips_airpurifier_coap/__init__.py
@@ -187,12 +187,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = config_entry_data
 
-    # await coordinator.async_first_refresh()
-    # _LOGGER.debug("coordinator did first refresh for host %s", host)
-
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
